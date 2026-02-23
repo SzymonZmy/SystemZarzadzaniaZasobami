20f9e0e716739abf0401243b404d07b8374c5320
Szymon <z.szymon.zm@gmail.com>
2026-02-23 12:34:04 +0100

    Add resources controller

---
+++ ResourceManagement.Api/Controllers/ResourcesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ResourceManagement.Api.Models;
+
+namespace ResourceManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Route("api/resources")]
+    public class ResourcesController : ControllerBase
+    {
+        private static List<Resource> resources = new();
+
+        [HttpGet]
+        public IEnumerable<Resource> Get()
+        {
+            return resources;
+        }
+
+        [HttpPost]
+        public void Post(Resource resource)
+        {
+            resources.Add(resource);
+        }
+    }
+}
