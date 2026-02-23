c80727dd1bfaba052084f261f2d0686c8fbf41a4
Szymon <z.szymon.zm@gmail.com>
2026-02-23 12:31:17 +0100

    Add resource model

---
+++ ResourceManagement.Api/Models/Resource.cs
@@ -0,0 +1,10 @@
+﻿namespace ResourceManagement.Api.Models
+{
+    public class Resource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
